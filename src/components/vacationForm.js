e759ca10ffb7f8d627539e5f5ee5dc6b1de7fe07
RoshdiAlMajzoub <rka35@mail.aub.edu>
2023-03-28 12:24:04 +0300

    2nd update

--- src/components/vacationForm.js
+++ src/components/vacationForm.js
@@ -3,6 +3,9 @@ import "./calander.css"
 import "bootstrap/dist/css/bootstrap.min.css";
 import {useState} from 'react';
 import Calendar from 'react-calendar'; 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Select, { components } from "react-select";
 
 
 
@@ -12,10 +15,21 @@ function VacationForm ()
         const [endDate, setEndDate] = useState(new Date(Date.now() + (3600 * 1000 * 24)))
         const [yearlyVacation,setYearlyVation] = useState(false)
         const [sickLeave,setSickLeave] = useState(false)
-        const [email,setEmail] = useState()
+        const [email,setEmail] = useState("")
         const [errEmail,setErrorEmail] = useState(false)
-        const [phoneNumber,setPhoneNumber] = useState()
+        const [phoneNumber,setPhoneNumber] = useState("")
         const [errPhoneNumber,setErrPhoneNumber] = useState(false)
+        const [selectedSickLeave,setSelectedSickLeave] = useState()
+
+        const options = [
+            { value: "1", label: "1" },
+            { value: "2", label: "2" },
+            { value: "3", label: "3" },
+            { value: "4", label: "4" },
+          ];
+
+        
+       
     
 
         const handleYearlyVacation = (event)=>{
@@ -35,48 +49,108 @@ function VacationForm ()
             setEndDate(new Date(date))
         }
 
-        const validateEmail =(event) => {
+        const validateEmail =(em) => {
             const regex = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
-            var isValid =  regex.test(event.target.value);
+            var isValid =  regex.test(em);
             
             if (isValid)
             {
-                setEmail(event.target.value)
                 setErrorEmail(false)
+                return true
             }
             else{
+                toast.error("Invalid email format")
                 setErrorEmail(true)
+                return false
             }
         }
 
-        const validatePhoneNumber =(event) => {
-            const phone = event.target.value
+        const validatePhoneNumber =(ph) => {
+            const phone = ph
             const splittedPair = phone.substring(0,2)
             const arr = ["03","81","70","71","76","78","79"]
             
-            if (phone.length ==8 && arr.includes(splittedPair))
+            if (phone.length ==8 && arr.includes(splittedPair) )
             {
-                setPhoneNumber(phone)
                 setErrPhoneNumber(false)
+                return true
             }else{
+                toast.error("Invalid Phone Number")
                 setErrPhoneNumber(true)
+                return false
             }
         }
 
+        const validateVacation = () => {
+            if (!sickLeave && !yearlyVacation)
+            {
+                toast.error("select vacation type")
+                return false
+            }
+            return true
+        }
+
+        const handleSickLeaveDays = (selectedOption) => {
+            setSelectedSickLeave(selectedOption.value)
+        }
+
+        
+
         const submitForm = (event) => {
             event.preventDefault();
-            if (!errEmail && !errPhoneNumber)
+            var emailValid = validateEmail(email);
+            var phoneValid = validatePhoneNumber(phoneNumber);
+            var vacationValid = validateVacation();
+            if (emailValid && phoneValid && vacationValid)
             {
-                console.log("submitted")
-            }else{
-                console.log("not submitted")
+                var obj = 
+                {
+                    "EmployeeId":1,
+                    "VacationType": "Yearly",
+                    "From": startDate,
+                    "To": endDate,
+                    "NumberOfDays": "5",
+                    "Email": email,
+                    "PhoneNumber": phoneNumber,
+                    "Name": "roshdi",
+                    "Status":"t"
+                }
+
+                const id = toast.loading("Please wait...")
+
+                fetch('https://localhost:7146/VacationRequest', {  
+
+                method: 'POST', 
+                mode: 'cors', 
+                body: JSON.stringify(obj) ,
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                  },
+
+                }).then( (response) => response.json())
+                .then((data) => {
+                   console.log(data);
+                   toast.update(id, {render: data["message"], type: "success", isLoading: false});
+                   // Handle data
+                })
+                .catch((err) => {
+                    console.log(err)
+                    toast.update(id, {render: err["message"], type: "error", isLoading: false});;
+                });
+
+
+
+                
             }
         }
         
 
       
         return(
-            <form>
+            
+            <form className='w-25'>
+                <ToastContainer />
+                <div className='bg-dark  p-3 rounded'>
                 <div className="form-group">
                     <label htmlFor="validationEmploeyeeName" className='align-self-start'>Name:</label>
                     <input type="text" className="form-control is-valid" id="validationEmploeyeeName" placeholder="Full Name" required></input>
@@ -84,12 +158,12 @@ function VacationForm ()
 
                 <div className="form-group">
                     <label htmlFor="validationEmail">Email:</label>
-                    <input type="text" className={ errEmail? "form-control is-valid border-danger" : "form-control is-valid"} id="validationEmail" onChange={validateEmail} placeholder="abc@gmail.com" required></input>
+                    <input type="text" className={ errEmail? "form-control is-valid border-danger" : "form-control is-valid"} id="validationEmail" value={email} onChange={(event)=>{setEmail(event.target.value)}} placeholder="abc@gmail.com" required></input>
                 </div>
 
                 <div className="form-group">
                     <label htmlFor="validationPhoneNumber">Phone Number:</label>
-                    <input type="text" className={ errPhoneNumber? "form-control is-valid border-danger" : "form-control is-valid"} id="validationPhoneNumber" onChange={validatePhoneNumber} placeholder="81234567" required></input>
+                    <input type="text" className={ errPhoneNumber? "form-control is-valid border-danger" : "form-control is-valid"} id="validationPhoneNumber" value={phoneNumber} onChange={(event)=>{setPhoneNumber(event.target.value)}} placeholder="81234567" required></input>
                 </div>
 
                 <p>Type of vacation:</p>
@@ -120,21 +194,19 @@ function VacationForm ()
                 
                 <label htmlFor="sVacation">Sick leave</label><br/>
                 {sickLeave &&
-                <select className='form-select form-select-lg mb-3'> 
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                </select>
+                
+                <Select
+                onChange={handleSickLeaveDays}
+                className="text-dark"
+                options={options}
+                />
                 }
                 <br></br>
 
 
-                <button className="btn btn-primary" onClick={submitForm}>Submit form</button>
+                <input type="submit" className="btn btn-primary" onClick={submitForm}></input>
 
-            
+                </div>
             </form>
         )
 }
